718685621b8903c9332f063112f81791274a7346
rokvisi <rokas.post@gmail.com>
2021-10-22 20:05:15 +0300

    Api testing.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import nProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { StoreProvider } from "../components/Store";
+import Router from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+Router.events.on("routerChangeStart", () => nProgress.start());
+Router.events.on("routerChangeComplete", () => nProgress.done());
+Router.events.on("routerChangeError", () => nProgress.done());
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  };
+};
